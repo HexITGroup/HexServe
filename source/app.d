ae88a7d0753e5317748b21cedf7845496db3e108
Clipsey <clipsey@localhost.localdomain>
2018-09-06 21:40:51 +0200

    basic web server (nonconfigurable)

--- source/app.d
+++ source/app.d
@@ -1,6 +1,18 @@
 import std.stdio;
+import vibe.http.fileserver;
+import vibe.http.server;
+import vibe.http.router;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	auto settings = new HTTPServerSettings;
+	settings.port = 80;
+	settings.bindAddresses = ["::1", "127.0.0.1"];
+
+	auto router = new URLRouter;
+	router.get("/", (scope HTTPServerRequest req, scope HTTPServerResponse res) => { res.redirect("/index.html"); });
+
+	router.get("*", serverStaticFiles("/var/www/"));
+
+	listenHTTP(settings, router);
 }
